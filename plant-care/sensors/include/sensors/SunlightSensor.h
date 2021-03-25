6a41478ae395eb7950818362f06b0210eea923c0
sajith <sajith@digitalxlabs.com>
2021-03-25 20:56:58 +0530

    Added plant care project to main project

---
+++ plant-care/sensors/include/sensors/SunlightSensor.h
@@ -0,0 +1,8 @@
+//
+// Created by sajith on 3/25/21.
+//
+
+#ifndef SOLID_SUNLIGHTSENSOR_H
+#define SOLID_SUNLIGHTSENSOR_H
+
+#endif //SOLID_SUNLIGHTSENSOR_H
---
+++ plant-care/sensors/src/MoistureSensor.cpp
@@ -0,0 +1,4 @@
+//
+// Created by sajith on 3/25/21.
+//
+
