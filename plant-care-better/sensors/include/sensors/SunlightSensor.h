f2a87acb3dff1fb4cb9bbb4a2d72c75bf25fb414
sajith <sajith@digitalxlabs.com>
2021-04-20 21:53:28 +0530

    Improved

--- plant-care-better/sensors/include/sensors/SunlightSensor.h
+++ plant-care-better/sensors/include/sensors/SunlightSensor.h
@@ -5,4 +5,40 @@
 #ifndef SOLID_SUNLIGHTSENSOR_H
 #define SOLID_SUNLIGHTSENSOR_H
 
+#include <chrono>
+#include <mutex>
+#include <optional>
+#include <set>
+
+#include "caretaker/WindowDevice.h"
+
+namespace sensors
+{
+    class SunlightSensor
+    {
+        using TimePoint = decltype(std::chrono::system_clock::now());
+
+        const std::chrono::seconds sleepTime;
+        std::mutex mtx;
+        std::set<interfaces::WindowDevice *> device;
+
+        std::optional<TimePoint> sunlightOnFrom;
+        std::optional<TimePoint> sunlightOffFrom;
+
+        const int threshold = 2;
+        bool sensorOn = true;
+
+    public:
+        SunlightSensor(const std::chrono::seconds, std::mutex &);
+        void subscribe(interfaces::WindowDevice &);
+        void operator()();
+
+    private:
+        void updateState(const bool);
+        bool isTooMuchSunlight(const bool);
+        bool isTooLittleSunlight(const bool);
+        bool isSunlight() const;
+    };
+}
+
 #endif //SOLID_SUNLIGHTSENSOR_H
