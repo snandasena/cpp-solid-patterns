aa8f2ec3fd6af647082170a371f76c6880a70830
sajith <sajith@digitalxlabs.com>
2021-04-25 12:48:34 +0530

    Improved

--- exceptions/intro/Exceptions_1.cpp
+++ exceptions/intro/Exceptions_1.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
 
+struct my_exception
+{
+};
+
+void foo()
+{
+//    throw -1;
+    throw my_exception{};
+}
+
 int main()
 {
+    int arr[] = {-1, 2, 4};
+
     try
     {
-        throw -1;
+//        throw -1;
+//        foo();
+        throw *arr;
     }
     catch (int e)
     {
-        std::cerr << e << '\n';
+        std::cerr << "Error - " << e << '\n';
+    } catch (const my_exception &e)
+    {
+        std::cerr << "my exception\n";
+    } catch (int *e)
+    {
+        std::cerr << "Error - " << e << '\n';
     }
 
     return 0;
